fa97f955003db32193cd024a0cd4f43c26de41bf
emmanuelromero <emmanuelromero@EMMANUELROM8AC2>
2025-06-18 10:46:38 -0300

    Fix: change Dni property type from double to long

--- ShoppingCart.Application/Dtos/CartDto.cs
+++ ShoppingCart.Application/Dtos/CartDto.cs
@@ -3,7 +3,7 @@
 public class CartDto
 {
     public int Id { get; set; }
-    public double UserDni { get; set; }
+    public long UserDni { get; set; }
     public List<CartItemDto> Items { get; set; } = new();
     public int TotalItems { get; set; }
     public decimal Total { get; set; }    
--- ShoppingCart.Domain/Entities/User.cs
+++ ShoppingCart.Domain/Entities/User.cs
@@ -3,7 +3,7 @@
 public class User
 {
     public int Id { get; set; }
-    public required double Dni { get; set; }
+    public required long Dni { get; set; }
     public required string Name { get; set; }
     public required bool IsVip { get; set; }
 }
